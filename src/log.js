1ff97ae240c3029048c45d263e8f066474dbd6e4
Renaud Vincent <renaudv@momentfactory.com>
2019-06-19 15:50:09 -0400

    Export declaration mismatch

--- src/log.js
+++ src/log.js
@@ -3,14 +3,15 @@
 /* eslint-disable prefer-spread */
 /* eslint-disable func-names */
 /* eslint-disable no-multi-assign */
-const shouldLog = module.exports.shouldLog = function (level) {
-  return log.level === true || log.level >= level;
-};
 
 const log = module.exports = function (level) {
   if (shouldLog(level)) {
     console.log.apply(console, Array.prototype.slice.call(arguments, 1));
   }
+};
+
+const shouldLog = module.exports.shouldLog = function (level) {
+  return log.level === true || log.level >= level;
 };
 
 log.level = 1;
